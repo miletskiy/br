eb41a4aed7391cf5e98636270efb9d1900e04f3f
miletskiy <s.miletskiy@gmail.com>
2016-12-14 20:51:12 +0200

    jsClasswork1:first js lesson

---
+++ jsClasswork1/js/main.js
@@ -0,0 +1,20 @@
+// first task
+alert("Я люблю БОРЩ!");
+
+// second task
+var admin;
+var name;
+
+name = "Василий";
+admin = name;
+
+alert(admin);
+
+// third task
+var planet = "Earth";
+var visitor = "Петя";
+
+// forth task
+var user_name = prompt("What is your name?", visitor);
+
+alert("User name is " + user_name);
---
+++ jsClasswork1/js/main1.js
@@ -0,0 +1,31 @@
+// Additional task
+var numberOne = 42;
+var numberTwo = 24;
+
+document.write(numberOne+numberTwo+"<br>");
+document.write(numberOne-numberTwo+"<br>");
+document.write(numberOne/numberTwo+"<br>");
+document.write(numberOne*numberTwo+"<br><hr>");
+
+// Task 3
+var integerNumber1 = 15;
+var integerNumber2 = 25;
+var integerNumber3 = 35;
+
+document.write((integerNumber1+integerNumber2+integerNumber3)/3+"<br><hr>");
+
+// Task 4
+
+function volumeCylinder(radius, height){
+	return Math.PI*radius*radius*height;
+};
+
+function squareCylinder(radius, height){
+	return 2*Math.PI*radius*(radius+height);
+};
+
+var user_radius = prompt("Please enter a radius", 10);
+var user_height = prompt("Please enter a height", 15);
+
+document.write("Volume of the cylinder "+volumeCylinder(user_radius, user_height)+"<br>");
+document.write("Square of the cylinder "+squareCylinder(user_radius, user_height));
