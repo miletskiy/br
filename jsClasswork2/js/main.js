a24850d673d68df85e2ee072ecb42b73653dc1d3
miletskiy <s.miletskiy@gmail.com>
2016-12-17 11:48:23 +0200

    Added first task

---
+++ jsClasswork2/js/main.js
@@ -0,0 +1,7 @@
+
+var user_number1 = prompt("Please enter the first number", "");
+var user_number2 = prompt("Please enter the second number", "");
+
+var sum = Number(user_number1) + Number(user_number2);
+
+document.write("The sum is " + sum);
