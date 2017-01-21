89047dcacf7d2cec2333f5cba4e1e75c4015c0e5
miletskiy <s.miletskiy@gmail.com>
2017-01-21 14:44:54 +0200

    Working version

--- jsClasswork2017_2/js/main.js
+++ jsClasswork2017_2/js/main.js
@@ -1,12 +1,17 @@
-
 // Slick slider
 
 
-$(document).ready(
-  //   function(){
-  // $('.your-class').slick({
-  //   setting-name: setting-value
-  // });
-// }
-);
+$(document).ready(function() {
+    // $("").css("background", "red");
+    $("#sample").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay:true,
+        dots: true,
+        infinite: true,
+        cssEase: 'linear',
+        arrows:true,
+        // nextArrow:"<button type="button" class="slick-next">Next</button>",
+    });
 
+});
