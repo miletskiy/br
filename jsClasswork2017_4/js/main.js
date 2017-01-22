0303c10ee7e4f495d8c4c52cf27a81b795dc3974
miletskiy <s.miletskiy@gmail.com>
2017-01-23 01:22:51 +0200

    routes:Completed task

--- jsClasswork2017_4/js/main.js
+++ jsClasswork2017_4/js/main.js
@@ -5,20 +5,65 @@
 
 var centerMap = { lat: 48.379557, lng: 31.165632 },
     map;
+var directionsDisplay;
+var directionsService;
+var autocompleteStart,autocompleteEnd;
 
-// styles from https://snazzymaps.com/style/25/blue-water
-var blueWater = [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#46bcec" }, { "visibility": "on" }] }];
+// styles from https://snazzymaps.com/style/83/muted-blue
+var mutedBlue = [{"featureType":"all","stylers":[{"saturation":0},{"hue":"#e7ecf0"}]},{"featureType":"road","stylers":[{"saturation":-70}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"simplified"},{"saturation":-60}]}];
 
 function initMap() {
 
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsService = new google.maps.DirectionsService();
     map = new google.maps.Map(document.getElementById('map'), {
         center: centerMap,
-        zoom: 7,
-        styles: blueWater
+        zoom: 6,
+        styles: mutedBlue
+    });
+    directionsDisplay.setMap(map);
+
+    var inputStart = document.getElementById("start");
+    var inputEnd = document.getElementById("end");
+    autocompleteStart = new google.maps.places.Autocomplete(inputStart);
+    autocompleteEnd = new google.maps.places.Autocomplete(inputEnd);
+    autocompleteStart.bindTo('bounds', map);
+    autocompleteEnd.bindTo('bounds', map);
+
+}
+
+
+function setRoute(start, end) {
+
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+        } else {
+            alert("Возникла ошибка:" + status);
+        }
     });
 }
 
-window.onload = function() {
-    console.log("Loaded!");
 
+window.onload = function() {
+
+    document.querySelector("#route").addEventListener("click", function() {
+        s = autocompleteStart.getPlace();
+        var latStart = s.geometry.location.lat();
+        var lngStart = s.geometry.location.lng();
+        e = autocompleteEnd.getPlace();
+        var latEnd = e.geometry.location.lat();
+        var lngEnd = e.geometry.location.lng();
+        start = new google.maps.LatLng(latStart, lngStart);
+        end = new google.maps.LatLng(latEnd, lngEnd);
+
+        setRoute(start, end);
+    });
+
+    console.log("Loaded!");
 }
