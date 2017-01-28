950a4a833c49f577c139e54c22393b89d62a5df1
miletskiy <s.miletskiy@gmail.com>
2017-01-28 13:30:57 +0200

    Add slider,images. Completed header

--- corporate/js/main.js
+++ corporate/js/main.js
@@ -3,16 +3,15 @@
 
 $(document).ready(function() {
     // $("").css("background", "red");
-    $("#sample").slick({
-        slidesToShow: 3,
+    $("#slider").slick({
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay:true,
-        dots: true,
+        autoplaySpeed:3000,
+        // dots: true,
         infinite: true,
         cssEase: 'linear',
-        arrows:true,
-        nextArrow:'<span><i class="fa fa-arrow-circle-right fa-2x" aria-hidden="true"></i></span>',
-        prevArrow: '<button type="button" data-role="none" class="slick-pre slick-arro" aria-label="Previous" role="button" style=""><i class="fa fa-asterisk" aria-hidden="true"></i></button>'
+        arrows:false,
     });
 
 });
