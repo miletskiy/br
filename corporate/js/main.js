23f45f80e5afafa74348b6d588d58a1b2d4d5450
miletskiy <s.miletskiy@gmail.com>
2017-02-01 00:01:20 +0200

    corporate:add isotope

--- corporate/js/main.js
+++ corporate/js/main.js
@@ -2,16 +2,30 @@
 
 
 $(document).ready(function() {
-    // $("").css("background", "red");
+
     $("#slider").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed:3000,
-        // dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
         infinite: true,
         cssEase: 'linear',
-        arrows:false,
+        arrows: false,
     });
 
+    // isotope settings
+    var select = "*";
+    var opts = {
+        itemSelector: ".ma-grid__item",
+        layoutMode: "fitRows",
+        filter: select
+    }
+    var $grid = $(".ma-grid").isotope(opts);
+
+    $(".ma-button").on("click", function() {
+        var filterVal = $(this).data().filter;
+        $grid.isotope({
+            filter: filterVal
+        });
+    });
 });
